9e99b64c564980b4aafcb92ad6666e18492c5a48
ddanmuji <cgyeo5533@gmail.com>
2022-12-14 16:08:52 +0900

    feat: client eslint fix

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import ItemsPage from './pages/ItemsPage';
+import PartPage from './pages/PartPage';
+import RidingStylesPage from './pages/RidingStylesPage';
+import './App.css';
+import './styles.css';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Header />
+      <section className="mx-auto mt-16">
+        <Switch>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/items" element={<ItemsPage />} />
+          <Route path="/part" element={<PartPage />} />
+          <Route path="/riding-styles" element={<RidingStylesPage />} />
+        </Switch>
+      </section>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
