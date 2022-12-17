610621b91d9707daff192774c7f5badd349fff71
ddanmuji <cgyeo5533@gmail.com>
2022-12-17 19:16:51 +0900

    feat: 메인 페이지 이미지 레이지로딩 적용

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -6,7 +6,8 @@ import main3 from '../assets/images/main3.jpg';
 import main_items from '../assets/images/main-items.jpg';
 import main_parts from '../assets/images/main-parts.jpg';
 import main_styles from '../assets/images/main-styles.jpg';
-import { BannerVideo, Card, Meta, ThreeColumns, TwoColumns } from '../components';
+import { BannerVideo, Card, Image, Meta, ThreeColumns, TwoColumns } from '../components';
+import { ROUTER_PATH } from '../constants';
 
 const MainPage = () => {
   return (
@@ -29,43 +30,37 @@ const MainPage = () => {
         <TwoColumns
           bgColor={'#f4f4f4'}
           columns={[
-            <img key={nanoid()} src={main_items} />,
+            <Image key={nanoid()} src={main_items} />,
             <Meta
               key={nanoid()}
-              title={'Items'}
-              content={
-                '롱보드는 기본적으로 데크가 크기 때문에 입맛에 따라 정말 여러가지로 변형된 형태가 나올수 있습니다. 실제로 데크마다 가지는 모양, 재질, 무게는 천차만별인데, 본인의 라이딩 스타일에 맞춰 롱보드를 구매하시는게 좋습니다.'
-              }
-              btnLink={'/items'}
+              title="Items"
+              content="롱보드는 기본적으로 데크가 크기 때문에 입맛에 따라 정말 여러가지로 변형된 형태가 나올수 있습니다. 실제로 데크마다 가지는 모양, 재질, 무게는 천차만별인데, 본인의 라이딩 스타일에 맞춰 롱보드를 구매하시는게 좋습니다."
+              btnLink={ROUTER_PATH.ITEMS}
             />,
           ]}
         />
         <TwoColumns
-          bgColor={'#fafafa'}
+          bgColor="#fafafa"
           columns={[
             <Meta
               key={nanoid()}
-              title={'Parts of Longboard'}
-              content={
-                '롱보드는 데크, 트럭, 휠, 킹핀, 베어링 등 여러 부품들로 구성됩니다. 롱보드를 타다보면 조금씩 고장나는 부품이 있기 마련인데, 이럴때를 위해 롱보들의 부품들에 대해서 알고 있으면 큰 도움이 됩니다.'
-              }
-              btnLink={'/part'}
+              title="Parts of Longboard"
+              content="롱보드는 데크, 트럭, 휠, 킹핀, 베어링 등 여러 부품들로 구성됩니다. 롱보드를 타다보면 조금씩 고장나는 부품이 있기 마련인데, 이럴때를 위해 롱보들의 부품들에 대해서 알고 있으면 큰 도움이 됩니다."
+              btnLink={ROUTER_PATH.PART}
             />,
-            <img key={nanoid()} src={main_parts} />,
+            <Image key={nanoid()} src={main_parts} />,
           ]}
           mobileReverse={true}
         />
         <TwoColumns
-          bgColor={'#f4f4f4'}
+          bgColor="#f4f4f4"
           columns={[
-            <img key={nanoid()} src={main_styles} />,
+            <Image key={nanoid()} src={main_styles} />,
             <Meta
               key={nanoid()}
-              title={'Riding Styles'}
-              content={
-                '롱보드 라이딩 스타일에는 크게 프리스타일, 다운힐, 프리라이딩, 댄싱이 있습니다. 보통 롱보드는 라이딩 스타일에 따라 데크의 모양이 조금씩 달라집니다. 많은 롱보드 매니아들이 각 쓰임새에 맞는 보드들을 소유하고 있습니다.'
-              }
-              btnLink={'/riding-styles'}
+              title="Riding Styles"
+              content="롱보드 라이딩 스타일에는 크게 프리스타일, 다운힐, 프리라이딩, 댄싱이 있습니다. 보통 롱보드는 라이딩 스타일에 따라 데크의 모양이 조금씩 달라집니다. 많은 롱보드 매니아들이 각 쓰임새에 맞는 보드들을 소유하고 있습니다."
+              btnLink={ROUTER_PATH.RIDING_STYLES}
             />,
           ]}
         />
